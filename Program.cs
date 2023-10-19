47ea70a579f6f7f689fa517f2f0a1b1be80a0921
Christina <nemovakg@gmail.com>
2023-10-19 23:20:52 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -2,22 +2,49 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+// class Work
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.Write("Введите число: ");
+//         int num = Convert.ToInt32(Console.ReadLine());
+//         Recurs(num);
+//     }
+//     public static void Recurs(int a)
+//     {
+//         if (a > 0)
+//         {
+//             Console.Write($"{a} ");
+//             a = a - 1;
+//             Recurs(a);
+//         }
+//     }
+// }
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
 class Work
 {
     public static void Main(string[] args)
     {
-        Console.Write("Введите число: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-        Recurs(num);
+        Console.Write("Введите число M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (m < n)
+        Console.WriteLine($"Сумма всех натуральных чисел в промежутке от {m} до {n} равна: {SumBetween(m, n)}");
+        else 
+        Console.WriteLine($"Сумма всех натуральных чисел в промежутке от {m} до {n} равна: {SumBetween(n, m)}");
     }
-    public static void Recurs(int a)
+    public static int SumBetween(int a, int b)
     {
-        if (a > 0)
+        int sum = 0;
+        for (int i = a; i <= b; i++)
         {
-            Console.Write($"{a} ");
-            a = a - 1;
-            Recurs(a);
+            sum += i;
         }
+        return sum;
     }
 }
-
